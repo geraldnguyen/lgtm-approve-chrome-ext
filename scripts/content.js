525f4dc3f7489d50726df72ff025bc32c1bfe6f4
Gerald Nguyen <ngthehuy@gmail.com>
2023-01-14 16:38:27 +0800

    Change button text and add comment

---
+++ scripts/content.js
@@ -0,0 +1,47 @@
+let approveBtn = undefined;
+let approveBtnSpan = undefined;
+let notYetApproved = undefined;
+
+const approveBtnClickListener = addComment('LGTM');
+
+window.addEventListener("load", scanAndSetup);
+
+function scanAndSetup() {
+  const intervalMs = 3 * 1000;  
+  const doOrDont = () => {
+    const attempted = attemptRename();
+    if (attempted) {
+      clearInterval(interval);
+    }
+  }
+  const interval = setInterval(doOrDont, intervalMs);
+}
+
+function getApproveBtn() {
+  return document.querySelector('button[data-qa-selector=approve_button]');
+}
+
+function attemptRename() {
+  approveBtn = getApproveBtn();
+  approveBtnSpan = approveBtn?.querySelector('span');
+  notYetApproved = 'Approve' === approveBtnSpan?.innerText;
+  
+  if (notYetApproved) {
+    approveBtnSpan.textContent = 'Look Good To Me!';
+    approveBtnSpan.addEventListener('click', approveBtnClickListener);
+  }
+  return !!approveBtn;
+}
+
+function addComment(text) {
+  return () => {
+    const commentTA = document.querySelector('#note-body');
+    commentTA.value = text;
+    commentTA.dispatchEvent(new Event('change'));
+    
+    setTimeout(() => {
+      const commentBtn = document.querySelector('div[data-qa-selector=comment_button]>button');
+      commentBtn.click();
+    }, 500);
+  };
+}
